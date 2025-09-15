86f8070ffcf7249a2a0615f40a8754683033e023
xzymon <xzymon@gmail.com>
2025-09-15 17:17:11 +0200

    Initial

---
+++ src/main/java/com/xzymon/ngsf/concepts/NgsfConceptsBeApplication.java
@@ -0,0 +1,13 @@
+package com.xzymon.ngsf.concepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgsfConceptsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgsfConceptsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xzymon/ngsf/concepts/NgsfConceptsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzymon.ngsf.concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgsfConceptsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
